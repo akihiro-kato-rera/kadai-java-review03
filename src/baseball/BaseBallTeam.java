9064910b2d4463a4268c43aa039d2340b76419e1
akihiro-kato-rera <tainakaritu19910821@yahoo.co.jp>
2025-04-29 06:30:26 +0900

    レッスン５の課題

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,33 @@
+package baseball;
+
+public class BaseBallTeam {
+
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    public BaseBallTeam() {
+
+    }
+
+    public BaseBallTeam(String name,int win,int lose,int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+
+    }
+
+
+    public void report() {
+        System.out.println(name + "の２０２２年の成績は" + win + "勝"+lose+"敗"+draw+"分、勝率は");
+        this.getRate();
+
+    }
+    public void getRate() {
+        double rate = (double)win / (win +lose);
+        System.out.println(rate+"です。");
+
+    }
+}
